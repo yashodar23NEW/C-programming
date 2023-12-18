c855c225cd18b230ed0b39b24cad63a1ef037bce
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-12-18 21:30:31 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int int1,int2;
+   printf("enter two integers");
+   scanf("%d %d",&int1,&int2);
+   if(int1==int2)
+   {
+
+       printf("number 1 and number 2 are equal\n");
+   }else
+   {
+      printf("number 1 and number 2 are not  equal");
+   }
+
+    return 0;
+}
