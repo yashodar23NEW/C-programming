693c5189481aaec222dcd01202d8f1a550e0e9ab
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-12-19 12:08:45 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, result;
+
+    printf("Input an integer : ");
+    scanf("%d", &num1);
+    result = num1 % 2;
+    if (rem1 == 0)
+        printf("%d is an even integer\n", num1);
+    else
+        printf("%d is an odd integer\n", num1);
+    return 0;
+}
