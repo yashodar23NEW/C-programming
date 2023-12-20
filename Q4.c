521dd3d8e71983f3a6cdde116af2dd3eb7f4dcc4
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-12-20 09:53:04 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    printf("enter the year");
+    scanf("%d",&year);
+    if(year %400==0)
+    {
+
+        printf("%d is a leap year\n",year);
+    }
+    else if(year%100==0)
+        {
+        printf("%d is not a leap year\n",year);
+    }
+    else if(year%4==0)
+    {
+
+        printf("%d is a leap year\n",year);
+    }
+    else{
+        printf("%d is not a leap year\n ",year);
+    }
+    return 0;
+}
